965ef79ecee822645d13dad4979c83359f69cd66
TMCooper <pokemon160377@gmail.com>
2025-09-30 14:00:50 +0200

    Update test

--- auto/update_readme.py
+++ auto/update_readme.py
@@ -4,19 +4,24 @@ import random
 # Les images disponibles
 images = [
     "./images/Frieren/frieren_meme.gif",
-    "./images/burnice/burnice_meme.gif"
+    "./images/burnice/burnice_meme.gif",
 ]
 
-# Lire le README
+# Lire le contenu du README
 with open("README.md", "r", encoding="utf-8") as f:
     content = f.read()
 
-# Choisir une image aléatoire (ou basée sur l’heure)
+# Choisir une nouvelle image au hasard
 new_image = random.choice(images)
 
-# Remplacer l'ancienne image par la nouvelle
-updated = re.sub(r'!\[.*?\]\(.*?\)', f'![]({new_image})', content)
+pattern = r'(## <img src=")(.*?)(" width="50"> About me)'
+replacement = r'\1' + new_image + r'\3'
 
-# Sauvegarder
+# Appliquer le remplacement
+updated_content = re.sub(pattern, replacement, content, flags=re.MULTILINE)
+
+# Sauvegarder les modifications dans le README
 with open("README.md", "w", encoding="utf-8") as f:
-    f.write(updated)
+    f.write(updated_content)
+
+print(f"README updated with image: {new_image}")
